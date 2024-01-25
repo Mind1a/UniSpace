8847a72ca8da11e5e0aa34935d660416e36fa93a
Davit Jabushanuri <davitjabushanuri95@gmail.com>
2024-01-25 21:10:57 +0400

    add header, sidebar and footer to layout

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,6 +1,62 @@
 import React from "react";
-import { SContainer } from "./Layout.styled";
+import {
+  SContainer,
+  SHeader,
+  SMain,
+  SSideBar,
+  SFooter,
+  SFooterLines,
+} from "./Layout.styled";
+import { FourthHeader } from "../Headers/FourthHeader";
+import { SecondHeader } from "../Headers/SecondHeader";
+import { Footer } from "../Footer";
+import { SideBar } from "../SideBar";
+import { useLocation } from "react-router-dom";
+import { ThirdHeader } from "../Headers/ThirdHeader";
+import { Header } from "../Headers/Header";
 
 export const Layout = ({ children }) => {
-  return <SContainer>{children}</SContainer>;
+  const { pathname } = useLocation();
+
+  return (
+    <SContainer>
+      <SHeader>
+        <div className="header">
+          {pathname === "/" ? (
+            <ThirdHeader />
+          ) : pathname === "/authentication" ||
+            pathname === "/recovery-password" ? (
+            <Header />
+          ) : pathname === "/registration" || pathname === "/privacy-policy" ? (
+            <SecondHeader />
+          ) : (
+            <FourthHeader />
+          )}
+        </div>
+        <div className="line" />
+      </SHeader>
+
+      <SMain>{children}</SMain>
+
+      <SSideBar>
+        <SideBar />
+      </SSideBar>
+
+      <SFooter>
+        <SFooterLines>
+          <div>
+            <span className="line" />
+            <span className="circle" />
+          </div>
+          <div>
+            <span className="circle" />
+            <span className="line" />
+          </div>
+        </SFooterLines>
+        <div className="footer">
+          <Footer />
+        </div>
+      </SFooter>
+    </SContainer>
+  );
 };
