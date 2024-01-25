94a864b1a116fbda4af098f8e0dff8a87ae1507a
Davit Jabushanuri <davitjabushanuri95@gmail.com>
2024-01-25 21:10:23 +0400

    refactor headers to work with new layout

--- src/components/Headers/Header/Header.styled.js
+++ src/components/Headers/Header/Header.styled.js
@@ -4,6 +4,5 @@ export const SHeader = styled.header`
   display: flex;
   justify-content: center;
   align-items: flex-end;
-  width: 100%;
   min-height: 10.5rem;
 `;
--- src/components/Headers/SecondHeader/SecondHeader.styled.js
+++ src/components/Headers/SecondHeader/SecondHeader.styled.js
@@ -3,8 +3,5 @@ import styled from "styled-components";
 export const SSecondHeader = styled.header`
   display: flex;
   align-items: center;
-  width: 100%;
   min-height: 10.5rem;
-  box-shadow: 0rem 0rem 0.125rem 0.063rem #ecececd4;
-  padding: 0 4.5rem;
 `;
