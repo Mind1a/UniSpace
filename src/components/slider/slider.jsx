5dc390cfc50f7cbea7ee5f9bb101da8b52d11eaa
Davit Jabushanuri <davitjabushanuri95@gmail.com>
2024-02-01 01:45:56 +0400

    create a slider

---
+++ src/components/slider/slider.jsx
@@ -0,0 +1,75 @@
+import { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+
+import { SSlider } from "./slider.styled";
+
+const variants = {
+  enter: (direction) => {
+    return {
+      x: direction > 0 ? 1000 : -1000,
+      opacity: 0,
+    };
+  },
+  center: {
+    zIndex: 1,
+    x: 0,
+    opacity: 1,
+  },
+  exit: (direction) => {
+    return {
+      zIndex: 0,
+      x: direction < 0 ? 1000 : -1000,
+      opacity: 0,
+    };
+  },
+};
+
+const swipeConfidenceThreshold = 10000;
+const swipePower = (offset, velocity) => {
+  return Math.abs(offset) * velocity;
+};
+
+const imageIndex = 0;
+
+export const Slider = ({ images }) => {
+  const [[page, direction], setPage] = useState([0, 0]);
+
+  const paginate = (newDirection) => {
+    setPage([page + newDirection, newDirection]);
+  };
+
+  return (
+    <>
+      <AnimatePresence initial={false} custom={direction}>
+        <SSlider>
+          <motion.img
+            key={page}
+            src={images[imageIndex]}
+            custom={direction}
+            variants={variants}
+            initial="enter"
+            animate="center"
+            exit="exit"
+            drag="x"
+            transition={{
+              x: { type: "spring", stiffness: 300, damping: 30 },
+              opacity: { duration: 0.2 },
+            }}
+            dragConstraints={{ left: 0, right: 0 }}
+            dragElastic={1}
+            onDragEnd={(e, { offset, velocity }) => {
+              const swipe = swipePower(offset.x, velocity.x);
+              if (swipe < -swipeConfidenceThreshold) {
+                paginate(1);
+              } else if (swipe > swipeConfidenceThreshold) {
+                paginate(-1);
+              }
+            }}
+            alt=""
+          />
+        </SSlider>
+        ;
+      </AnimatePresence>
+    </>
+  );
+};
