b9d1ec66fe43aaa3b473dfbd474712525b801fdb
Davit Jabushanuri <davitjabushanuri95@gmail.com>
2024-03-05 00:25:02 +0400

    create my-projects route

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,7 @@ import { UploadProjectAdmin } from "./views/UploadProjectAdmin";
 import dayjs from "dayjs";
 import "dayjs/locale/ka";
 import { Layout } from "./components/Layout";
+import { MyProjects } from "./views/MyProjects";
 
 dayjs.locale("ka");
 
@@ -40,6 +41,7 @@ const App = () => {
         <Route path="/upload-project-admin" element={<UploadProjectAdmin />} />
         <Route path="/portfolio" element={<Portfolio />} />
         <Route path="/project" element={<ProjectPage />} />
+        <Route path="/my-projects" element={<MyProjects />} />
       </Routes>
     </Layout>
   );
---
+++ src/views/MyProjects/MyProjects.jsx
@@ -0,0 +1,5 @@
+import { SMyProjects } from "./MyProjects.styled";
+
+export const MyProjects = () => {
+  return <SMyProjects>my projects</SMyProjects>;
+};
