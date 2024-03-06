194be1cf255705b85febeea7cf862c12c4376a36
TheLastDance <davit.khachaturov.1@iliauni.edu.ge>
2024-03-06 21:10:17 +0400

    updating CourseSlider component
    - updated styles for component's optimization
    - added own slider settings file
    - changed icons of dotted borders
    - updated settings of slider

--- src/components/CourseSlider/CourseSlider.jsx
+++ src/components/CourseSlider/CourseSlider.jsx
@@ -1,6 +1,6 @@
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import { settings } from "../ContributorsSlider/sliderSettings";
+import { settings } from "./CourseSliderSettings";
 import Slider from "react-slick";
 import {
   SCourseCard,
@@ -12,21 +12,15 @@ import {
   SVectorBottomRight,
   SVectorTopLeft,
 } from "./CourseSlider.styled"
-import { CustomPrevArrow } from "../ContributorsSlider/CustomArrows/CustomArrows";
-import { CustomNextArrow } from "../ContributorsSlider/CustomArrows/CustomArrows";
 import { Button } from "../Button";
-
-const options = {
-  ...settings,
-  prevArrow: <CustomPrevArrow right="7rem" />,
-  nextArrow: <CustomNextArrow right="2rem" />
-}
+import { useNavigate } from "react-router-dom";
 
 export const CourseSlider = ({ data, buttonContent }) => {
+  const navigate = useNavigate();
 
   return (
     <SCourseSlider>
-      <Slider {...options}>
+      <Slider {...settings}>
         {data.map(({ id, course, src }) => (
           <SCourseCard key={id}>
             <SCourseInfo>
@@ -35,31 +29,31 @@ export const CourseSlider = ({ data, buttonContent }) => {
                 <SVectorTopLeft>
                   <img src="assets/svg/course_card_dotted_line1.svg" />
                 </SVectorTopLeft>
-                <SVectorTopLeft left="-1rem" top="1rem">
+                <SVectorTopLeft left="1rem" top="1rem">
                   <img src="assets/svg/course_card_line1.svg" />
                 </SVectorTopLeft>
                 <SVectorBottomRight>
                   <img src="assets/svg/course_card_dotted_line2.svg" />
                 </SVectorBottomRight>
-                <SVectorBottomRight right="-1rem" bottom="1rem">
+                <SVectorBottomRight right="1rem" bottom="1rem">
                   <img src="assets/svg/course_card_line2.svg" />
                 </SVectorBottomRight>
               </SCourseImgContainer>
+              <SCourseFullname>
+                <span>{course}</span>
+              </SCourseFullname>
+              <Button
+                type="button"
+                margin="2.2rem 0 0.5rem 0"
+                border="1px solid #fff"
+                borderHover="1px solid #315c8e"
+                bgColor="transparent"
+                borderRadius="0.375rem"
+                onClick={() => navigate("/")}
+              >
+                {buttonContent}
+              </Button>
             </SCourseInfo>
-            <SCourseFullname>
-              <span>{course}</span>
-            </SCourseFullname>
-            <Button
-              type="button"
-              width="70%"
-              margin="2.2rem 0 0 0"
-              border="1px solid #fff"
-              borderHover="1px solid #315c8e"
-              bgColor="transparent"
-              borderRadius="0.375rem"
-            >
-              {buttonContent}
-            </Button>
           </SCourseCard>
         ))}
       </Slider>
